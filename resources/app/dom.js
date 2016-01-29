6d49e70c8e4832f1fd330d8bad2f2eb89807734c
Colton Wiens <XNBlank@users.noreply.github.com>
2016-01-29 02:59:44 -0500

    Added ability to use Custom CSS.

--- resources/app/dom.js
+++ resources/app/dom.js
@@ -1,11 +1,42 @@
 (function(){
 
+  var fs = require("fs");
   var shell = require("shell");
+
+  var datapath = process.env.HOME + "/.config/Discord/init.json";
+  var data;
+  try{
+    data = JSON.parse(fs.readFileSync(datapath, 'utf-8'));
+  } catch(e){
+    alert("Error loading init.json." + e);
+  }
+
+  var toggleCSS;
+  toggleCSS = data.useCSS;
+  //alert("Using CSS?" + toggleCSS);
+
   onload = function(){
+      //alert(csspath);
+      var customcss = "";
       var webview = document.getElementById("discord-webview");
+      if(toggleCSS == true){
+        var csspath = process.env.HOME + "/.config/Discord/user.css";
+	fs.readFile(csspath, 'utf-8', function(err,data){
+	  if (err) {alert(err);}
+	  customcss = data;
+	});
+      }
+      webview.addEventListener("dom-ready", function(event){
+	//webview.openDevTools();
+	if(customcss != "" && toggleCSS == true){
+	  webview.insertCSS(customcss);
+	}
+      });
+
       webview.addEventListener("new-window", function(event){
           console.log(event.url);
           shell.openExternal(event.url);
       });
+
   };
 })();
