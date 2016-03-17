2213febd3f42b4a8af309d169f30c9ba5f07f8a9
Colton Wiens <XNBlank@users.noreply.github.com>
2016-03-17 01:06:41 -0400

    Update main.js

--- resources/app/main.js
+++ resources/app/main.js
@@ -3,7 +3,7 @@ var BrowserWindow = require('browser-window');
 var Menu = require('menu');
 var MenuItem = require('menu-item');
 var Tray = require('tray');
-var ipc = require('electron').ipcMain;
+var ipc = require('electron').ipcMain
 var globalShortcut = require('global-shortcut');
 var shell = require('shell');
 var path = require("path");
@@ -18,15 +18,20 @@ catch(e){
 	data = {
 		"bounds": {"x":100, "y":100, "width":1024,"height":768},
 		"minTray":false,
-		"useCSS":false
+		"useCSS":false,
+		"isPTB":false,
+		"isCanary":false
 	};
 }
 
 var menu = new Menu();
 var minToTray;
 var useCustomCSS;
+var usePTB;
+var useCanary;
 minToTray = data.minTray;
 useCustomCSS = data.useCSS;
+usePTB = data.usePTB;
 var updateReady = false;
 var quitForReal = false; // Dirty
 
@@ -40,6 +45,12 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+
+    if((usePTB == true) && (useCanary == true)){
+	alert("You cannot have both Canary and PTB enabled. Disabling both.");
+	usePTB = false;
+	useCanary = false;
+    }
 
     updateWindow = new BrowserWindow({width: 300, height: 300, icon: __dirname + '/icon.png', title: "Discord", 'auto-hide-menu-bar': true, frame: false});
     updateWindow.setResizable(false);
@@ -60,6 +71,9 @@ app.on('ready', function() {
     mainWindow.setMenuBarVisibility(false);
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+      
+
+
     //Nullify any closed windows.
     mainWindow.on('closed', function() {
         //updateWindow.close();
@@ -73,7 +87,9 @@ app.on('ready', function() {
         var data = {
 					bounds: mainWindow.getBounds(),
 					minTray: minToTray,
-					useCSS: useCustomCSS
+					useCSS: useCustomCSS,
+					isPTB: usePTB,
+					isCanary: useCanary
         };
         fs.writeFileSync(initPath, JSON.stringify(data));
     });
@@ -120,9 +136,44 @@ app.on('ready', function() {
 						useCustomCSS = false;
 						console.log("Enabled useCustomCSS");
 					}
-					toggleCSS.checked = useCustomCSS;
+	
+			toggleCSS.checked = useCustomCSS;
+	
 				}
 			});
+
+    //PTB and CANARY toggles
+	var togglePTB = new MenuItem({
+		label: 'Use PTB Build',
+		type: 'checkbox',
+		checked: usePTB,
+		click: function() {
+			if(togglePTB.checked == true){
+				usePTB = true;
+				useCanary = false;
+			} else if(togglePTB.checked == false){
+				usePTB = false;
+			}
+		togglePTB.checked = usePTB;
+
+		}
+	});
+
+	var toggleCanary = new MenuItem({
+		label: 'Use Canary Build',
+		type: 'checkbox',
+		checked: useCanary,
+		click: function() {
+			if(toggleCanary.checked == true){
+				useCanary = true;
+				usePTB = false;
+			} else if(toggleCanary.checked == false){
+				useCanary = false;
+			}
+		toggleCanary.checked = useCanary;
+
+		}
+	});
 
     mainWindow.on('close', function(event) {
         if(minToTray && !quitForReal){
@@ -143,6 +194,8 @@ app.on('ready', function() {
     menu.append(new MenuItem({ type: 'separator' }));
     menu.append(disMinButton);
 		menu.append(toggleCSS);
+		menu.append(togglePTB);
+		menu.append(toggleCanary);
     menu.append(new MenuItem({ type: 'separator' }));
     menu.append(new MenuItem({ label: 'Quit Discord', type: 'normal', click: function() { quitForReal = true; app.quit(); } }));
 
@@ -178,9 +231,4 @@ app.on('ready', function() {
 		event.preventDefault();
 	});
 
-});
-
-ipc.on("resize-onload", function(){
-	mainWindow.setSize(data.bounds.width+1, data.bounds.height+1);
-	mainWindow.setSize(data.bounds.width, data.bounds.height);
 });
